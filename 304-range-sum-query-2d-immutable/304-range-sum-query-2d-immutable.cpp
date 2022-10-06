6905b43ba6cd4ce4de4c0914e1bb4800751cc953
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2022-10-06 23:00:58 +0300

    Time: 928 ms (20.52%), Space: 142.4 MB (93.23%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,28 +1,19 @@
 class NumMatrix {
-public:
-    vector<vector<int>> matrix;
-    int rows;
-    int columns;
-    NumMatrix(vector<vector<int>>& matrix) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=1;j<matrix[0].size();j++)
-                matrix[i][j]=matrix[i][j-1]+matrix[i][j];
-        }
-        this->matrix=matrix;
-    }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for(int i = row1;i<=row2;i++){
-            int left=0;
-            if(col1-1>=0)
-                left = matrix[i][col1-1];
-            sum+=matrix[i][col2]-left;
-        }
-        return sum;
-    }
-};
+    public:
+        vector<vector<int>> matrix;
 
+        NumMatrix(vector<vector<int>>& matrix) {
+            this->matrix=vector<vector<int>>(matrix.size()+1, vector<int>(matrix[0].size()+1, 0));
+            for(int i=1;i<=matrix.size();i++){
+                for(int j=1;j<=matrix[0].size();j++)
+                    this->matrix[i][j] = matrix[i-1][j-1] + this->matrix[i-1][j] + this->matrix[i][j-1] - this->matrix[i-1][j-1];
+            }        
+        }
+
+        int sumRegion(int row1, int col1, int row2, int col2) {
+            return matrix[row2+1][col2+1] - matrix[row1][col2 + 1] - matrix[row2 + 1][col1] + matrix[row1][col1];
+        }
+};
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix* obj = new NumMatrix(matrix);
