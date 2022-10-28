da83cdbe9ee157b373bd4b8e52844ee8b5405987
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2022-10-28 11:40:55 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (38.20%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0, b=1;
+        if(n==0) return a;
+        for(int i=2;i<=n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+};
