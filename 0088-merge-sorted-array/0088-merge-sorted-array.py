a0c559c7db0442288b2fea5db3981a4d4b6d80c7
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2022-12-30 09:26:07 +0300

    Time: 45 ms (71.22%), Space: 14 MB (37.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,16 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        queue = []
-        l, r= 0, 0
-        while(l<m+n):
-            nums1[l] = float('inf') if l >= m else nums1[l]
-            w = nums2[r] if r<n else float('inf')
-            if queue and queue[0] <= nums1[l] and queue[0] < w:
-                queue.append(nums1[l]) if nums1[l]!=float('inf') else None
-                nums1[l] = queue.pop(0)  
-            elif(w < nums1[l]):
-                queue.append(nums1[l]) if nums1[l]!=float('inf') else None
-                nums1[l] = nums2[r]
-                r+=1
-            l+=1
+        left, right = m-1, n-1
+        head = m+n-1
+        while left>=0 or right>=0:
+            left_val = nums1[left] if left>=0 else -float('inf')
+            right_val = nums2[right] if right>=0 else -float('inf')
+            
+            if left_val < right_val:
+                nums1[head] = right_val
+                right-=1
+            else:
+                nums1[head] = left_val
+                left-=1
+            
+            head -= 1
