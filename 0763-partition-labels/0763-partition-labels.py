63b3a47eb81fb8025f6bca6ddb6a77af3bae181d
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-01-25 22:27:27 +0300

    Time: 48 ms (51.26%), Space: 13.9 MB (61.28%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        rightMost = {}
+        
+        for i in range(1, len(s)+1):
+            if i > rightMost.get(s[i-1], 0):  
+                rightMost[s[i-1]] = i
+                
+        ans = []       
+        j, max_i, count = 1, 1, 0
+        while j<=len(s):
+            if rightMost[s[j-1]] > max_i:  
+                max_i=rightMost[s[j-1]]
+            if j==max_i:
+                ans.append(count+1)
+                count=-1
+            count+=1
+            j+=1
+        
+        return ans;
