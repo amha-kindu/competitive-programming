623b9347834c61f67fd54ef2e280ab487dd72a99
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-02-23 14:18:17 +0300

    Time: 1527 ms (40.93%), Space: 25 MB (40.87%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,15 +1,11 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.prefixSum2D = [[0 for _ in range(len(matrix[0])+1)]]
-        for i in range(len(matrix)):
-            temp = [0]
-            for j in range(len(matrix[0])):
-                temp.append(matrix[i][j])
-            self.prefixSum2D.append(temp)
+        r, c = len(matrix), len(matrix[0])
+        self.prefixSum2D = [[0]*(c+1) for _ in range(r+1)]
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        for i in range(r):
+            for j in range(c):
                 self.prefixSum2D[i+1][j+1] = self.prefixSum2D[i][j+1]+self.prefixSum2D[i+1][j]+matrix[i][j] - self.prefixSum2D[i][j]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
