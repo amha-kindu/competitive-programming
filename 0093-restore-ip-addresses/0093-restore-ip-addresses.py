4ad43b75b9e3d6fc01bc73b379d5d2763ad20431
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-03-22 09:57:12 +0300

    Time: 41 ms (50.00%), Space: 13.9 MB (73.98%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,21 +1,23 @@
 class Solution:
-    def getValidIps(self, s, i=0, ip=[]):
-        n = len(s)
-        
-        if i >= n:
-            if len(ip) == 4:
-                self.valid_ips.append('.'.join(ip))
-            return
-        
-        for j in range(i, n):
-            temp = s[i:j+1]
-            if (temp[0] != '0' or len(temp)==1) and int(temp) <= 255 and len(ip) < 4:
-                self.getValidIps(s, j+1, ip+[temp])
+    
 
     def restoreIpAddresses(self, s: str, ips=[]) -> List[str]:
-        self.valid_ips = []
+        valid_ips = []
+        n = len(s)
+        
+        def getValidIps(i=0, ip=[]):
 
-        self.getValidIps(s)
+            if i >= n:
+                if len(ip) == 4:
+                    valid_ips.append('.'.join(ip))
+                return
 
-        return self.valid_ips
+            for j in range(i, n):
+                temp = s[i:j+1]
+                if (temp[0] != '0' or len(temp)==1) and int(temp) <= 255 and len(ip) < 4:
+                    getValidIps(j+1, ip+[temp])
+
+        getValidIps()
+
+        return valid_ips
         
