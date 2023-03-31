d54ba9be7096185c59aa2cd3046c98a104507ef1
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-03-31 11:04:42 +0300

    Time: 35 ms (38.80%), Space: 13.8 MB (90.87%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,6 +1,3 @@
 class Solution:
-    def findComplement(self, num: int) -> int:
-        binary = list(bin(num)[2:])
-        for i in range(len(binary)):
-            binary[i] = str((int(binary[i])+1) % 2)
-        return int(''.join(binary), 2)
+    def findComplement(self, num: int) -> int:     
+        return num ^ (2 ** int.bit_length(num) - 1)
