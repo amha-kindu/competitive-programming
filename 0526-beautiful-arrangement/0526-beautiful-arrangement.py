84fe5c7909217b69cc2a250cb542088236616601
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-04-03 16:19:42 +0300

    Time: 2388 ms (5.10%), Space: 13.9 MB (68.25%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,19 +1,20 @@
 class Solution:
-    def computePermutation(self, nums, n=0, path_len=0):
-        len_ = len(nums)
-        if path_len == len_:
-            self.beautiful_arrangements += 1
-            return
-
-        for i in range(len_):
-            # perform kth bit test0
-            ith_bit_off = n & (1 << i) == 0
-
-            index = path_len+1
-            if ith_bit_off and (nums[i] % index == 0 or index % nums[i] == 0):
-                self.computePermutation(nums, n+2**i, path_len+1)
 
     def countArrangement(self, n: int) -> int:
         self.beautiful_arrangements = 0
-        self.computePermutation([i for i in range(1, n+1)])
+        nums = [i for i in range(1, n+1)]
+        
+        def computePermutation(num=0, path_len=0):
+            if path_len == n:
+                self.beautiful_arrangements += 1
+                return
+
+            for i in range(n):
+                # perform kth bit test0
+                ith_bit_off = num & (1 << i) == 0
+
+                index = path_len+1
+                if ith_bit_off and (nums[i] % index == 0 or index % nums[i] == 0):
+                    computePermutation(num+2**i, path_len+1)
+        computePermutation()
         return self.beautiful_arrangements
