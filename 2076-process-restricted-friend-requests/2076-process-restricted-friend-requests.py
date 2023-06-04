c20a546234e1be613a3a1feba0c21523c3842b6a
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-06-04 10:11:14 +0300

    Time: 4851 ms (44.76%), Space: 17.3 MB (25.87%) - LeetHub

---
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -0,0 +1,60 @@
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        answer = []
+        friends = DisjointSet(n)
+        for p1, p2 in requests:
+            possible = True
+            p1_friends = set(friends.components(p1))
+            p2_friends = set(friends.components(p2))
+            for src, tgt in restrictions:
+                if src in p1_friends and tgt in p2_friends or\
+                    src in p2_friends and tgt in p1_friends:
+                    possible = False
+                    break
+            if possible:
+                friends.union(p1, p2)
+            answer.append(possible)
+        return answer
+
+
+class DisjointSet:
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+        self.rank = [0 for i in range(n)]
+    
+    def rep(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.rep(self.root[x])
+        return self.root[x]
+    
+    def getReps(self):
+        return [i for i in range(len(self.root)) if self.root[i]==i]
+    
+    def areConnected(self, x, y):
+        return self.rep(x) == self.rep(y)
+    
+    def allComponents(self):
+        return [self.components(rep) for rep in self.getReps()]
+
+    def components(self, x):
+        root = self.rep(x)
+        answer = []
+        for i in range(len(self.root)):
+            if self.rep(i) == root:
+                answer.append(i)
+        return answer
+
+    def union(self, x, y):
+        rootX = self.rep(x)
+        rootY = self.rep(y)
+
+        if rootX == rootY:
+            return
+        
+        if self.rank[rootX] > self.rank[rootY]:
+            self.root[rootY] = rootX
+        elif self.rank[rootX] < self.rank[rootY]:
+            self.root[rootX] = rootY
+        else:
+            self.rank[rootX] += 1
+            self.root[rootY] = rootX
