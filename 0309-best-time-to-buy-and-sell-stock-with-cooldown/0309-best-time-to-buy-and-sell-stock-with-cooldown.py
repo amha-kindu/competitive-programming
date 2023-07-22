9fe135f556d3200c5bcd0ebb1269230f2c4e6368
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-07-22 23:29:32 +0300

    Time: 48 ms (95.60%), Space: 16.6 MB (91.81%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,13 +4,11 @@ class Solution:
     hold = -float('inf')
     prev = 0
 
-    # Loop through each price in the prices array
     for price in prices:
       cache = sell
       sell = max(sell, hold + price)
       hold = max(hold, prev - price)
-      # Update the previous sell value to be the cached sell value from the beginning 
-      # of the iteration
+
       prev = cache
 
     return sell
